b7d72ce304fd267d7cc9546661f70f04fed9ecab
Shashank Tiwari <shashanktiwari1312@gmail.com>
2023-09-21 23:00:49 +0530

    created the footer component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+        <p>Musicart | All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer;
